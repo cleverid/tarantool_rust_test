7a713e189256954dd0a772949444fee27eeed7d2
Шапошников Евгений <shaposhnikovea@lad24.ru>
2023-10-30 17:58:24 +0300

    batch transaction test

---
+++ src/domain/item.rs
@@ -0,0 +1,10 @@
+#[derive(Copy, Clone, Debug)]
+pub struct Item {
+    id: String,
+    group_id: String,
+    title: string,
+    start_time: u32,
+    end_time: u32,
+}
+
+impl Item {}
--- src/lib.rs
+++ src/lib.rs
@@ -9,19 +9,21 @@ fn insert() -> Result<bool, String> {
     let plan_item_space = Space::find("plan_item").ok_or_else(|| "Can't find space plan_item".to_string())?;
 
     let start_time = Utc::now().time();
-    transaction(|| -> Result<(), Error> {
-        for _ in 1..100_000 {
-            let id = nanoid!();
-            let title = "Название работы".to_string();
-            let group_id = nanoid!();
-            let start_time = Utc::now().timestamp_micros();
-            let end_time = start_time + 1000 * 60 * 60 * 24;
-            let row = (id, title, group_id, start_time, end_time);
-            plan_item_space.replace(&row)?;
-        }
-        Ok(())
-    })
-    .unwrap();
+    for _ in 1..10 {
+        transaction(|| -> Result<(), Error> {
+            for _ in 1..10_000 {
+                let id = nanoid!();
+                let title = "Название работы".to_string();
+                let group_id = nanoid!();
+                let start_time = Utc::now().timestamp_micros();
+                let end_time = start_time + 1000 * 60 * 60 * 24;
+                let row = (id, title, group_id, start_time, end_time);
+                plan_item_space.insert(&row)?;
+            }
+            Ok(())
+        })
+        .unwrap();
+    }
 
     let end_time = Utc::now().time();
     let diff = end_time - start_time;
